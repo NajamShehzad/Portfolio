6ef6545caceb3e3448f29a4bd535497dc325ea16
NajamShehzad <najambutt195@gmail.com>
2020-12-06 01:05:48 +0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.a3893f44df34680cab2cf272a648997e.js"
+  "/Portfolio/precache-manifest.424c52e6708f74e922e295d90502d61e.js"
 );
 
 self.addEventListener('message', (event) => {
