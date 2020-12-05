635313e309fb073ddfc33e9baff390129a1d5044
NajamShehzad <najambutt195@gmail.com>
2020-12-06 01:30:13 +0500

    ReactGA

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import ReactGA from 'react-ga';
 import logo from "./logo.svg";
 import "./App.css";
 import Main from "./containers/Main";
-
+ReactGA.initialize("UA-130800032-1");
 function App() {
   return (
     <div>
