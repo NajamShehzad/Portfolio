d68d0e864e9280396345cfab01eca4ce72762608
NajamShehzad <najambutt195@gmail.com>
2021-03-25 13:16:44 +0000

    Deploying to gh-pages from  @ f5026f00cce3758d2d346996ddacf2f724a3293e 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.229a677fd41fa03b594e29689faf91e4.js"
+  "/precache-manifest.4a4bcd6426726fd4c2afcaccf9a67ea2.js"
 );
 
 self.addEventListener('message', (event) => {
